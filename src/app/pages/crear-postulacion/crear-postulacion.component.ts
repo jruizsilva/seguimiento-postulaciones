4efdd6fa8bd0edac27d8878862f93243224035c2
jruizsilva <ruizsilvajonathan@gmail.com>
2024-02-05 17:11:39 -0300

    layout crear postulacion done

--- src/app/pages/crear-postulacion/crear-postulacion.component.ts
+++ src/app/pages/crear-postulacion/crear-postulacion.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIcon } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-crear-postulacion',
   standalone: true,
-  imports: [MatToolbarModule],
+  imports: [
+    MatToolbarModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIcon,
+    MatButtonModule,
+  ],
   templateUrl: './crear-postulacion.component.html',
   styleUrl: './crear-postulacion.component.scss',
 })
